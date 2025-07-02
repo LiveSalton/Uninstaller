ca5b51eb039dbb4f315c4c4eebbbfbc5c77b1db5
LiveSalton <50036757+LiveSalton@users.noreply.github.com>
2025-07-02 17:40:49 +0800

    clean

--- app/src/main/java/com/salton123/uninstaller/PreloadCore.java
+++ app/src/main/java/com/salton123/uninstaller/PreloadCore.java
@@ -25,8 +25,6 @@ public enum PreloadCore {
     public ExecutorService mThreadPool = Executors.newFixedThreadPool(8);
     private List<AppEntity> mInstallPackagesInfos = new ArrayList<>();
 
-    private List<AppEntity> mSystemPackagesInfos = new ArrayList<>();
-
     public List<AppEntity> preloadAppList() {
         Log.i("PreloadCore", "start preloadAppList time=" + System.currentTimeMillis());
         FutureTask task = new FutureTask<>(new Callable<List<AppEntity>>() {
@@ -62,45 +60,7 @@ public enum PreloadCore {
         }
     }
 
-    public List<AppEntity> preloadSystemAppList() {
-        Log.i("PreloadCore", "start preloadAppList time=" + System.currentTimeMillis());
-        FutureTask task = new FutureTask<>(new Callable<List<AppEntity>>() {
-            @Override
-            public List<AppEntity> call() throws Exception {
-                Log.i("PreloadCore", "start call time one=" + System.currentTimeMillis());
-                PackageManager pm = XApp.getInstance().getPackageManager();//获取包管理者
-                List<AppEntity> pList = new ArrayList<>();// 获取所有的应用程序集合
-                for (PackageInfo item : pm.getInstalledPackages(0)) {
-                    AppEntity entity = new AppEntity(item);
-                    pList.add(entity);
-                    if (!Utils.isThirdPartyApp(item.applicationInfo)) {
-                        mSystemPackagesInfos.add(entity);
-                    }
-                }
-                Log.i("PreloadCore", "start call time=" + System.currentTimeMillis());
-                return pList;
-            }
-        });
-        //不采用线程池是因为要提高执行优先级和速度
-        // mThreadPool.submit(task);
-        Thread thread = new Thread(task);
-        thread.setPriority(10);
-        thread.start();
-        try {
-            mSystemPackagesInfos = (List<AppEntity>) task.get();
-            Log.i("PreloadCore", "start get time=" + System.currentTimeMillis());
-            return mSystemPackagesInfos;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ArrayList<>();
-        }
-    }
-
     public List<AppEntity> getInstalledPackages() {
         return mInstallPackagesInfos;
-    }
-
-    public List<AppEntity> getSystemPackages() {
-        return mSystemPackagesInfos;
     }
 }
